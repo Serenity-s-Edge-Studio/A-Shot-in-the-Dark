530163d418e23f9d3bed1f99f36fe04ee0b5ebd8
sholom1 <42785937+sholom1@users.noreply.github.com>
2020-11-21 16:17:11 -0500

    Zombies now try to follow the lights

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,5 +4,7 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public List<LightDrop> influencingLights = new List<LightDrop>(); 
+    public List<LightDrop> influencingLights = new List<LightDrop>();
+    public Vector2 target;
+    public float timeTillNextRandom;
 }

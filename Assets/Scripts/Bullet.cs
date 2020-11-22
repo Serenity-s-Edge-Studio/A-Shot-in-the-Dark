24d0fe5fbb18d13461971b825d13fb0b255bf874
sholom1 <42785937+sholom1@users.noreply.github.com>
2020-11-21 19:32:45 -0500

    Update Bullet.cs

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,20 +6,13 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField]
     private float damage;
-    [SerializeField]
-    private int penetration;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Zombie") && collision.TryGetComponent<Enemy>(out Enemy enemy))
         {
-            penetration--;
-            if (penetration > 0)
             enemy.Damage(damage);
-            if (penetration <= 0)
-            {
-                Destroy(transform.parent.gameObject, 0.2f);
-                this.enabled = false;
-            }
+            Destroy(transform.parent.gameObject, 0.2f);
+            gameObject.SetActive(false);
         }
     }
 }

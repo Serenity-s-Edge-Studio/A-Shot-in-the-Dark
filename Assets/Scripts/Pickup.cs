bd24fcd90f97a19829c16308467c17bd166c5aa2
sholom1 <42785937+sholom1@users.noreply.github.com>
2020-11-21 22:44:19 -0500

    Added gun prefabs

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    [SerializeField]
+    private Type type;
+    [SerializeField]
+    private int max;
+    [SerializeField]
+    private int clipSize;
+    [SerializeField]
+    private int fireRate;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && collision.TryGetComponent<Player>(out Player player))
+        {
+            player.equipWeapon(type, Random.Range(clipSize, max), clipSize, fireRate);
+            Destroy(gameObject);
+        }
+    }
+    public enum Type
+    {
+        Pistol,
+        Shotgun, 
+        GlowstickLauncher,
+        Machinegun,
+        Flamethrower
+    }
+}

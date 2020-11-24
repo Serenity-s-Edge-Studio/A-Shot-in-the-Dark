436f290c01c563bd24e59d9db740463a6f438997
sholom1 <42785937+sholom1@users.noreply.github.com>
2020-11-24 01:45:21 -0500

    Migrated Gamemanager and settings menu to a new Persistent Scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -19,16 +19,36 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI volumeText;
     public static GameManager instance;
+    private int CurrentSceneIndex = 1;
     private void Awake()
     {
         instance = this;
+#if !UNITY_EDITOR
+        LoadScene(1);
+#else
+        if (SceneManager.GetSceneByBuildIndex(CurrentSceneIndex).isLoaded)
+        {
+            var buttonList = GetAllObjectsOnlyInSceneWithTag<Button>("LoadGameButton");
+            if (buttonList.Count > 0)
+            {
+                Button loadGameButton = buttonList[0];
+                loadGameButton.onClick.AddListener(() => LoadScene(2));
+            }
+            else
+            {
+                Debug.LogWarning("Could not finds settings button in loaded scenes");
+            }
+        }
+#endif
     }
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(gameObject);
-        DontDestroyOnLoad(settingsGO);
+#if UNITY_EDITOR
+        FindAndUpdateSettingsButton();
+#else
         updateSettings();
+#endif
     }
     public void updateSettings()
     {
@@ -38,15 +58,45 @@ public class GameManager : MonoBehaviour
     }
     public void LoadScene(int index)
     {
-        SceneManager.LoadSceneAsync(index).completed += GameManager_completed;
+        LoadScene(index, null);
+    }
+    public void LoadScene(int index, System.Action<AsyncOperation>[] onComplete)
+    {
+        if (SceneManager.GetSceneByBuildIndex(CurrentSceneIndex).isLoaded)
+            SceneManager.UnloadSceneAsync(CurrentSceneIndex);
+        AsyncOperation LoadSceneOperation = SceneManager.LoadSceneAsync(index, LoadSceneMode.Additive);
+        if (onComplete != null)
+        {
+            foreach (System.Action<AsyncOperation> operation in onComplete)
+            {
+                LoadSceneOperation.completed += operation;
+            }
+        }
+        LoadSceneOperation.completed += GameManager_completed;
+        CurrentSceneIndex = index;
     }
 
     private void GameManager_completed(AsyncOperation obj)
     {
-        Button settingsButton = GetAllObjectsOnlyInSceneWithTag<Button>("SettingsButton")[0];
-        settingsButton.onClick.AddListener(() => settingsGO.SetActive(!settingsGO.activeInHierarchy));
+        SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(CurrentSceneIndex));
+        FindAndUpdateSettingsButton();
+    }
+
+    private void FindAndUpdateSettingsButton()
+    {
+        var buttonList = GetAllObjectsOnlyInSceneWithTag<Button>("SettingsButton");
+        if (buttonList.Count > 0)
+        {
+            Button settingsButton = buttonList[0];
+            settingsButton.onClick.AddListener(() => settingsGO.SetActive(!settingsGO.activeInHierarchy));
+        }
+        else
+        {
+            Debug.LogWarning("Could not finds settings button in loaded scenes");
+        }
         updateSettings();
     }
+
     public List<T> GetAllObjectsOnlyInScene<T>() where T : Object
     {
         List<T> objectsInScene = new List<T>();

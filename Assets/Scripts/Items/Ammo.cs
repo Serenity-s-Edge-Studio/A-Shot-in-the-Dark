fdf40aacbd0882e95e61a2857145c7108d106886
Sam Kessler <sholomkessler1@gmail.com>
2021-02-16 23:00:51 -0500

    Added item scriptable objects

--- Assets/Scripts/Items/Ammo.cs
+++ Assets/Scripts/Items/Ammo.cs
@@ -7,18 +7,4 @@ public class Ammo : Item
     public string caliber;
     public int damage;
     public LightDrop BulletPrefab;
-
-    //public Ammo(string name, string description, float mass, string caliber, int damage, string bulletName)
-    //    : base(name, description, mass)
-    //{
-    //    this.caliber = caliber;
-    //    this.damage = damage;
-    //    this.BulletPrefab = Resources.Load<LightDrop>("Items/Projectiles/" + bulletName);
-    //    if (BulletPrefab == null)
-    //    {
-    //        if (BulletPlaceholder == null)
-    //            BulletPlaceholder = Resources.Load<LightDrop>("Items/Projectiles/Bullet");
-    //        this.BulletPrefab = BulletPlaceholder;
-    //    }
-    //}
 }

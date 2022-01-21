500ea42915bd1f6f12b503aa29e85c00b6a42289
Sam Kessler <42785937+sholom1@users.noreply.github.com>
2022-01-21 00:10:00 -0500

    Started on RTS mode

---
+++ Assets/Scripts/Player/Tools/SelectUnits.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Player.Tools
+{
+    public class SelectUnits : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
